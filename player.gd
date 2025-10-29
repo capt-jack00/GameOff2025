58050d59b11ca16a54f783f7c0f42cc3e2bec282
Kociarz <dbednorz@tuta.io>
2025-10-29 17:19:06 +0100

    Player movement added

---
+++ player.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+var speed = 100
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	if Input.is_action_pressed("move_right"):
+		input_vector.x += 1
+		$Sprite2D.flip_h = false
+		$AnimationPlayer.play("soldier_walk")
+	if Input.is_action_pressed("move_left"):
+		input_vector.x -= 3
+		$Sprite2D.flip_h = true
+		$AnimationPlayer.play("soldier_walk")
+	if Input.is_action_pressed("move_down"):
+		input_vector.y += 1
+	if Input.is_action_pressed("move_up"):
+		input_vector.y -= 1
+	if input_vector == Vector2.ZERO:
+		$AnimationPlayer.play("soldier_idle")
+		print("Player is not moving")
+
+		
+		
+	input_vector = input_vector.normalized()
+	
+	velocity = input_vector * speed
+	
+	move_and_slide()
