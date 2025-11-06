b0808578b871a0c09f7502cb621b5c3beb5ef98a
dawid <dawidmakarewicz74@gmail.com>
2025-11-06 18:17:41 +0100

    Sword hitbox changes position based on player rotation Player now can both attack and move in the same frame

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,46 +3,55 @@ extends CharacterBody2D
 var speed = 2
 @export var attack_damage := 10
 
-#TODO: Sword hitbox doesn't change directions when player walking does. Fix that.
+#TODO: 
 
+func update_attack_hitbox(direction):		#updates sword hitbox based on player rotation
+	var offset = 16
+	$attack_hitbox.position.x = 0			#return to basic value so the hitbox doesn't go away
+	$attack_hitbox.position.x += offset * direction
+	
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
-	
-	if $AnimationPlayer.is_playing() and $AnimationPlayer.current_animation == "p_attack":
-		move_and_collide(Vector2.ZERO)
-		return
 
-	if Input.is_action_pressed("move_right"):
+	if Input.is_action_pressed("move_right"):	#D
 		input_vector.x += 1
 		$Sprite2D.flip_h = false
-		$AnimationPlayer.play("soldier_walk")
 		
-	if Input.is_action_pressed("move_left"):
-		input_vector.x -= 3
+	if Input.is_action_pressed("move_left"):	#A
+		input_vector.x -= 1
 		$Sprite2D.flip_h = true
-		$AnimationPlayer.play("soldier_walk")
 		
-	if Input.is_action_pressed("move_down"):
+	if Input.is_action_pressed("move_down"):	#S
 		input_vector.y += 1
-		$AnimationPlayer.play("soldier_walk")
 		
-	if Input.is_action_pressed("move_up"):
+	if Input.is_action_pressed("move_up"):		#W
 		input_vector.y -= 1
-		$AnimationPlayer.play("soldier_walk")
-		
-	if input_vector == Vector2.ZERO:
-		$AnimationPlayer.play("soldier_idle")
+	
 	
 	# --- ATTACK ---
+	if input_vector.x != 0.0:		#check for 0 is needed so later it doesn't multiply by 0
+		update_attack_hitbox(input_vector.x)	
+		
 	if Input.is_action_just_pressed("left_mb"):
+		$attack_hitbox.monitoring = true
 		$AnimationPlayer.play("p_attack")
+		await $AnimationPlayer.animation_finished
+		$attack_hitbox.monitoring = false
 		print("Attack!")
+		
+	#	==================	Animation handling	=========================
+	if !$AnimationPlayer.current_animation == "p_attack":	
+		if input_vector == Vector2.ZERO:
+			$AnimationPlayer.play("soldier_idle")
+		else:
+			$AnimationPlayer.play("soldier_walk")
+
 
 	input_vector = input_vector.normalized()
 	velocity = input_vector * speed
 	move_and_collide(velocity)
+	
 
-
-func _on_p_attack_hitbox_area_entered(area: Area2D) -> void:
+func _on_attack_hitbox_area_entered(area: Area2D) -> void:
 	if area.is_in_group("hurtbox"):
 		area.get_parent().take_damage(attack_damage)
