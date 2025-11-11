d7e500bd55f63c5702666ca6f8ea3d4c1a14ec59
dawid <dawidmakarewicz74@gmail.com>
2025-11-11 17:01:47 +0100

    Maybe fixed old version

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -2,8 +2,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	get_tree().change_scene_to_file("res://scenes/world.tscn") # skip main menu (usefull for debugging)
-	print_rich("[color=red]Menu was turned off in file main_menu.gd on line 6 to debug game easier[/color]")
+	#get_tree().change_scene_to_file("res://scenes/world.tscn") # skip main menu (usefull for debugging)
+	#print_rich("[color=red]Menu was turned off in file main_menu.gd on line 6 to debug game easier[/color]")
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
