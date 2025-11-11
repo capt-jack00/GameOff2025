e1f56abbbfe59bb4d54f94048dd7e4c4d362dc60
dawid <dawidmakarewicz74@gmail.com>
2025-11-11 12:00:52 +0100

    Made that while player is attacking he cannot rotate sword hitbox

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,13 +4,12 @@ var speed = 5
 @export var attack_damage := 10
 var criticalStrikeChance := 5	# this value is % for a critical strike
 
-@export var inv: Inv
-
 func rotate_sword_hitbox():
 	var mousePos = get_viewport().get_mouse_position()
 	var windowSize = get_viewport().get_visible_rect().size
 	var mousePosAroundCenterOfScreen = windowSize / 2 - mousePos
-	$attack_hitbox.rotation = PI / 2 - atan(mousePosAroundCenterOfScreen.x / mousePosAroundCenterOfScreen.y)
+	if !$attack_hitbox.monitoring:
+		$attack_hitbox.rotation = PI / 2 - atan(mousePosAroundCenterOfScreen.x / mousePosAroundCenterOfScreen.y)
 	# equastion above works by calculating how far away from center of screen is mouse position and then
 	# doing reverse of trygonometric function(tan) to get angle at which should box be rotated
 	# '-' is here because it was rotating in wrong directionsd
@@ -18,17 +17,17 @@ func rotate_sword_hitbox():
 	# 90 degrees
 	# when you use tan on angle you get ratio of square triangle legs. Here is exactly the same process but
 	# in reverse.
-	var offset = 0
-	if mousePosAroundCenterOfScreen.y >= 0:	#set offset based on where the mouse is
-		$attack_hitbox/shape/StrikeSprite.flip_h = true
-		$attack_hitbox/shape/StrikeSprite.flip_v = true
-		offset = -25
-	else:
-		$attack_hitbox/shape/StrikeSprite.flip_h = false
-		$attack_hitbox/shape/StrikeSprite.flip_v = false
-		offset = 25
-	$attack_hitbox/shape.position.x = 0 
-	$attack_hitbox/shape.position.x += offset	
+		var offset = 0
+		if mousePosAroundCenterOfScreen.y >= 0:	#set offset based on where the mouse is
+			$attack_hitbox/shape/StrikeSprite.flip_h = true
+			$attack_hitbox/shape/StrikeSprite.flip_v = true
+			offset = -25
+		else:
+			$attack_hitbox/shape/StrikeSprite.flip_h = false
+			$attack_hitbox/shape/StrikeSprite.flip_v = false
+			offset = 25
+		$attack_hitbox/shape.position.x = 0 
+		$attack_hitbox/shape.position.x += offset	
 	
 func _physics_process(_delta):
 	var input_vector = Vector2.ZERO
@@ -52,20 +51,11 @@ func _physics_process(_delta):
 	move_and_slide()	#Moves player and doesn't block him on wall
 	
 	
+	
 	# --- ATTACK ---
+	handle_attack()
 	rotate_sword_hitbox()
-		
-	if Input.is_action_just_pressed("left_mb") && !$attack_hitbox.monitoring:
-		$AnimationPlayer.play("p_attack")			#play animation of player attacking
-		$attack_hitbox/shape/StrikeSprite.visible = true		#makes strike sprite visible (sprite in front of player)
-		$attack_hitbox/shape/AnimationPlayer.play("strike")		#plays animation of strike
-		
-		$attack_hitbox.monitoring = true
-		await $AnimationPlayer.animation_finished	
-		$attack_hitbox.monitoring = false
-		
-		$attack_hitbox/shape/StrikeSprite.visible = false
-		print("Attack!")
+	#handle_attack()
 		
 	#	==================	Animation handling	=========================
 	if $AnimationPlayer.current_animation != "p_attack":	# makes sure that attack doesn't get overwritten
@@ -73,8 +63,20 @@ func _physics_process(_delta):
 			$AnimationPlayer.play("soldier_idle")
 		else:
 			$AnimationPlayer.play("soldier_walk")
-	
 
-func _on_attack_hitbox_area_entered(area: Area2D) -> void:		#checks if sword hit box is inside another thing hurtbox
+func handle_attack():
+	if Input.is_action_just_pressed("left_mb") && !$attack_hitbox.monitoring:
+		$AnimationPlayer.play("p_attack")			#play animation of player attacking
+		$attack_hitbox/shape/StrikeSprite.visible = true		#makes strike sprite visible (sprite in front of player)
+		$attack_hitbox/shape/AnimationPlayer.play("strike")		#plays animation of strike
+		 
+		$attack_hitbox.monitoring = true
+		await $AnimationPlayer.animation_finished	
+		$attack_hitbox.monitoring = false
+		
+		$attack_hitbox/shape/StrikeSprite.visible = false
+		print("Attack!")
+
+func _on_attack_hitbox_area_entered(area: Area2D) -> void:		#chesdcks if sword hit box is inside another thing hurtbox
 	if area.is_in_group("hurtbox"):
 		area.get_parent().take_damage(attack_damage, criticalStrikeChance)
